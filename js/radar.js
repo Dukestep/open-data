028f8c39f04a90bf6b2b3823ae3be22b5ee1c6d8
etiennepelletier <etienne.pelletier@canada.ca>
2019-11-29 15:22:58 -0500

    version 2.1.8

--- js/radar.js
+++ js/radar.js
@@ -1,74 +1,38 @@
-"use strict";
+const parser = new DOMParser();
 
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
-
-function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
-
-var parser = new DOMParser();
-
-function getRadarStartEndTime() {
-  return _getRadarStartEndTime.apply(this, arguments);
+async function getRadarStartEndTime() {
+  let response = await fetch('https://geo.weather.gc.ca/geomet/?lang=en&service=WMS&request=GetCapabilities&version=1.3.0&LAYERS=RADAR_1KM_RSNO')
+  let data = await response.text().then(
+    data => parser.parseFromString(data, 'text/xml').getElementsByTagName('Dimension')[0].innerHTML.split('/')
+  )
+  return [new Date(data[0]), new Date(data[1])]
 }
 
-function _getRadarStartEndTime() {
-  _getRadarStartEndTime = _asyncToGenerator(
-  /*#__PURE__*/
-  regeneratorRuntime.mark(function _callee() {
-    var response, data;
-    return regeneratorRuntime.wrap(function _callee$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            _context.next = 2;
-            return fetch('https://geo.weather.gc.ca/geomet/?lang=en&service=WMS&request=GetCapabilities&version=1.3.0&LAYERS=RADAR_1KM_RSNO');
+let frameRate = 1.0; // frames per second
+let animationId = null;
+let current_time = null;
 
-          case 2:
-            response = _context.sent;
-            _context.next = 5;
-            return response.text().then(function (data) {
-              return parser.parseFromString(data, 'text/xml').getElementsByTagName('Dimension')[0].innerHTML.split('/');
-            });
+let layers = [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    }),
+    new ol.layer.Image({
+      source: new ol.source.ImageWMS({
+        format: 'image/png',
+        url: 'https://geo.weather.gc.ca/geomet/',
+        params: {'LAYERS': 'RADAR_1KM_RSNO', 'TILED': true},
+      })
+    }),
+    new ol.layer.Tile({
+      source: new ol.source.TileWMS({
+        format: 'image/png',
+        url: 'https://geo.weather.gc.ca/geomet/',
+        params: {'LAYERS': 'RADAR_COVERAGE_RSNO.INV', 'TILED': true},
+      })
+    })
+  ]
 
-          case 5:
-            data = _context.sent;
-            return _context.abrupt("return", [new Date(data[0]), new Date(data[1])]);
-
-          case 7:
-          case "end":
-            return _context.stop();
-        }
-      }
-    }, _callee);
-  }));
-  return _getRadarStartEndTime.apply(this, arguments);
-}
-
-var frameRate = 1.0; // frames per second
-
-var animationId = null;
-var current_time = null;
-var layers = [new ol.layer.Tile({
-  source: new ol.source.OSM()
-}), new ol.layer.Image({
-  source: new ol.source.ImageWMS({
-    format: 'image/png',
-    url: 'https://geo.weather.gc.ca/geomet/',
-    params: {
-      'LAYERS': 'RADAR_1KM_RSNO',
-      'TILED': true
-    }
-  })
-}), new ol.layer.Tile({
-  source: new ol.source.TileWMS({
-    format: 'image/png',
-    url: 'https://geo.weather.gc.ca/geomet/',
-    params: {
-      'LAYERS': 'RADAR_COVERAGE_RSNO.INV',
-      'TILED': true
-    }
-  })
-})];
-var map = new ol.Map({
+let map = new ol.Map({
   target: 'map',
   layers: layers,
   view: new ol.View({
@@ -78,43 +42,40 @@ var map = new ol.Map({
 });
 
 function updateInfo(current_time) {
-  var el = document.getElementById('info');
-  el.innerHTML = "Time / Heure (UTC): ".concat(current_time.toISOString());
+  let el = document.getElementById('info');
+  el.innerHTML = `Time / Heure (UTC): ${current_time.toISOString()}`
 }
 
 function setTime() {
-  current_time = current_time;
-  getRadarStartEndTime().then(function (data) {
+  current_time = current_time
+  getRadarStartEndTime().then(data => {
     if (current_time === null) {
       current_time = data[0];
     } else if (current_time >= data[1]) {
-      current_time = data[0];
+      current_time = data[0]
     } else {
       current_time = new Date(current_time.setMinutes(current_time.getMinutes() + 10));
     }
-
-    layers[1].getSource().updateParams({
-      'TIME': current_time.toISOString().split('.')[0] + "Z"
-    });
-    updateInfo(current_time);
-  });
+    layers[1].getSource().updateParams({'TIME': current_time.toISOString().split('.')[0]+"Z"});
+    updateInfo(current_time)
+  })
 }
-
 setTime();
 
-var stop = function stop() {
+let stop = function() {
   if (animationId !== null) {
     window.clearInterval(animationId);
     animationId = null;
   }
 };
 
-var play = function play() {
+let play = function() {
   stop();
   animationId = window.setInterval(setTime, 1000 / frameRate);
 };
 
-var startButton = document.getElementById('play');
+let startButton = document.getElementById('play');
 startButton.addEventListener('click', play, false);
-var stopButton = document.getElementById('pause');
+
+let stopButton = document.getElementById('pause');
 stopButton.addEventListener('click', stop, false);
